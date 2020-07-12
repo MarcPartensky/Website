872b888bd2ad2924843e1c0187b5dddf453c5898
Marc Partensky <marc.partensky@gmail.com>
2020-07-12 22:35:41 +0200

    redirected home

--- django_project/urls.py
+++ django_project/urls.py
@@ -31,9 +31,9 @@ urlpatterns = [
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
     path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
-    path('', include('blog.urls')),
+    path('blog/', include('blog.urls')),
     path('first-site/', include('first_site.urls')),
-    path('home/', include('home.urls')),
+    path('', include('home.urls')),
     path('demo/', include('demo.urls')),
     path('planning/', include('planning.urls')),
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 def home(request):
     context = dict(title='Website of Marc Partensky')
     return render(request, 'home/home.html', context)
+
+def about(request):
+    context = dict(title="About Marc Partensky")
+    return render(request, 'home/about.html', context)
