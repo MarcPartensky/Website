c5ec44e3a11eb7c0e7e9100d99cdee689fea0262
Marc Partensky <marc.partensky@gmail.com>
2020-07-27 01:57:51 +0200

    gameposts and awesome navbar

--- games/views.py
+++ games/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from .models import GamePost, GamePostComment
 # Create your views here.
 def games(request):
-    context = dict(title="games")
+    context = dict(
+        title="games",
+        gameposts=GamePost.objects.all(),
+        gamepostcomments=GamePostComment.objects.all(),
+    )
     return render(request, 'games/games.html', context=context)
