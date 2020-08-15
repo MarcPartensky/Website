d5c1753701a152fe42accce7bd676e6641ff4e8f
Marc Partensky <marc.partensky@gmail.com>
2020-08-15 22:40:49 +0200

    enhanced planning a lot

--- planning/views.py
+++ planning/views.py
@@ -3,6 +3,28 @@ from pyicloud import PyiCloudService
 import datetime
 import os
 
+def duration_printer(duration):
+    d = int(duration/24/60)
+    h = int(duration%(24*60)/60)
+    m = duration%60 
+    out = []
+    if duration >= 24 * 60 and d != 0:
+        if d >= 2:
+            out.append(f"{d} days")
+        else:
+            out.append(f"{d} day")
+    if duration >= 60 and h != 0:
+        if h >= 2:
+            out.append(f"{h} hours")
+        else:
+            out.append(f"{h} hour")
+    if m != 0:
+        if m >= 2:
+            out.append(f"{m} minutes")
+        else:
+            out.append(f"{m} minute")
+    return ' '.join(out)
+
 def get_events():
     api = PyiCloudService(os.environ['APPLE_MAIL'], os.environ['APPLE_PASSWORD'])
     date = datetime.datetime.now()
@@ -11,6 +33,7 @@ def get_events():
     raw_events = api.calendar.events(from_dt, to_dt)
 
     f = lambda date: f"{str(date[4]).zfill(2)}:{str(date[5]).zfill(2)}"
+    
     events = []
     for raw_event in raw_events:
         if date.day < raw_event['startDate'][3]:
@@ -20,10 +43,12 @@ def get_events():
             start = f(raw_event['startDate']),
             end = f(raw_event['endDate']),
             guid = raw_event['guid'],
+            duration = duration_printer(raw_event['duration']),
         )
         events.append(event)
     events.sort(key = lambda e:e['start'])
     return events
+        
 
 def planning(request):
     events = get_events()
