eb748cdee34b410ed94607e1362655eaa613ae69
Marc Partensky <marc.partensky@gmail.com>
2020-10-11 20:29:51 +0200

    changed my cv

--- project/views.py
+++ project/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def projects(request):
     render(request, 'projects/projects.html')
+
+def reversi(request):
+    render(request, 'projects/reversi.html')
