94cab538dd99a40248ee65c30dc3260242dad931
Marc Partensky <marc.partensky@gmail.com>
2020-11-15 21:18:12 +0100

    added example view

--- api/urls.py
+++ api/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('test', views.test, name='test'),
     path('random', views.random_req, name='random'),
     path('addition', views.addition, name='addition'),
+    path('example', views.example , name='example'),
     # path('user/create/', views.user.create, name="user-create"),
     # path('<str:room_name>/', views.room, name='room'),
 ]
