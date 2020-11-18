d6dadc5586963f879ee724b5bfe6bfbb8f93f33b
Marc Partensky <marc.partensky@gmail.com>
2020-11-18 12:35:01 +0100

    forgot to collect static as freaking usual

---
+++ staticfiles/article/index.73d9ce0034d1.js
@@ -0,0 +1,11 @@
+Dropzone.autoDiscover = false;
+$(function() {
+    const dropzone = new Dropzone("#dropzone");
+    // const dropzone = $("dropzone")
+
+    dropzone.on("success", function(file, responseText) {
+        window.location.href = `/article/${file.name.replace('.md', '')}`
+    });
+})
+
+
