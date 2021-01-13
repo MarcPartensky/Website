9fdd60e98d1281e0ff0308001360d0be412c527c
marc <marc.partensky@gmail.com>
2021-01-13 22:51:08 +0100

    mbeditor config

--- django_project/settings.py
+++ django_project/settings.py
@@ -353,6 +353,38 @@ REST_FRAMEWORK = {
     ]
 }
 
+MDEDITOR_CONFIGS = {
+    'default':{
+        'width': '100% ',  # Custom edit box width
+        'heigth': '100%',  # Custom edit box height
+        'toolbar': [
+            "undo", "redo", "|",
+            "bold", "del", "italic", "quote", "ucwords", "uppercase", "lowercase", "|",
+            "h1", "h2", "h3", "h5", "h6", "|",
+            "list-ul", "list-ol", "hr", "|",
+            "link", "reference-link", "image", "code", "preformatted-text", "code-block", "table", "datetime",
+            "emoji", "html-entities", "pagebreak", "goto-line", "|",
+            "help", "info",
+            "||", "preview", "watch", "fullscreen"
+        ],  # custom edit box toolbar
+        'upload_image_formats': ["jpg", "jpeg", "gif", "png", "bmp", "webp"],  # image upload format type
+        'image_folder': 'editor',  # image save the folder name
+        'theme': 'default',  # edit box theme, dark / default
+        'preview_theme': 'default',  # Preview area theme, dark / default
+        'editor_theme': 'default',  # edit area theme, pastel-on-dark / default
+        'toolbar_autofixed': True,  # Whether the toolbar capitals
+        'search_replace': True,  # Whether to open the search for replacement
+        'emoji': True,  # whether to open the expression function
+        'tex': True,  # whether to open the tex chart function
+        'flow_chart': True,  # whether to open the flow chart function
+        'sequence': True, # Whether to open the sequence diagram function
+        'watch': True,  # Live preview
+        'lineWrapping': False,  # lineWrapping
+        'lineNumbers': False,  # lineNumbers
+        'language': 'fr'  # zh / en / es
+    }
+}
+
 # SQL Explorer : https://pypi.org/project/django-sql-explorer/
 # EXPLORER_DEFAULT_CONNECTION = 'readonly'
 EXPLORER_CONNECTIONS = {'Default': 'default', 'Production': 'production'}
