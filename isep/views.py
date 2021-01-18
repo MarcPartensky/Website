2d26aa928ce3992b80be73653ed9a4a22438e788
marc <marc.partensky@gmail.com>
2021-01-18 19:47:11 +0100

    hydrated context for isep subfolder

--- isep/views.py
+++ isep/views.py
@@ -1,20 +1,31 @@
 from django.shortcuts import render
 
+from home.context import base, hydrate
+
 # Create your views here.
 
-def isep(request):
+@hydrate(base)
+def isep(request, context:dict={}):
+    """Affiche une page sur l'isep."""
     context = dict(isep='isep')
     return render(request, 'isep/isep.html', context)
 
-def app(request):
-    context = dict(app='app')
+@hydrate(base)
+def app(request, context:dict={}):
+    """Affiche les groupes d'App."""
+    context['app'] = 'app'
     return render(request, 'isep/app.html', context)
 
-def planning_app(request):
-    return render(request, 'isep/planning_app.html', {})
+@hydrate(base)
+def planning_app(request, context:dict={}):
+    """Affiche le planning d'App info."""
+    return render(request, 'isep/planning_app.html', context)
 
-def lignee(request):
-    return render(request, 'isep/lignee.html', {})
+@hydrate(base)
+def lignee(request, context:dict={}):
+    """Affiche les lignées à l'Isep."""
+    return render(request, 'isep/lignee.html', context)
 
-def garage(request):
-    return render(request, 'isep/garage.html', {})
+def garage(request, context:dict={}):
+    """Blaqgue pour le garage"""
+    return render(request, 'isep/garage.html', context)
