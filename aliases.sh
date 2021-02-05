8ea6d83267f9eea8d06ed5a3c95b1243e4885b55
marc <marc.partensky@gmail.com>
2021-02-05 14:48:52 +0100

    added servant alias

--- aliases.sh
+++ aliases.sh
@@ -5,6 +5,7 @@ alias dotaliases="v $DOTFILES_PATH/aliases.sh"
 alias dotmain="v $DOTFILES_PATH/main.sh"
 alias dotfunctions="v $DOTFILES_PATH/functions.sh"
 alias esclave="cd ~/discordbot/esclave"
+alias servant="cd ~/discordbot/servant-de-marc"
 alias esclave-aws="ssh -i 'discordbotofmarcpartensky.pem' ubuntu@ec2-15-236-33-61.eu-west-3.compute.amazonaws.com"
 alias startmamp='cd /Applications/MAMP/bin && ./start.sh'
 alias stopmamp='cd /Applications/MAMP/bin && ./stop.sh'
