0a41813a6cc2efb28eb13d1b5f3095e11eeb80d3
marc <marc.partensky@gmail.com>
2021-02-14 23:58:01 +0100

    share and fetch done

--- django_project/settings.py
+++ django_project/settings.py
@@ -270,7 +270,6 @@ STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    # '/var/www/static/',
 ]
 
 STATICFILES_FINDERS = [
--- file/views.py
+++ file/views.py
@@ -5,14 +5,14 @@ This app aims to give file access in a restricted way.
 
 
 import os
-import datetime
 
 from django.shortcuts import render, redirect, reverse
 from django.views.decorators.csrf import csrf_exempt
+from django.utils import timezone
 
 from django.http.response import HttpResponse, FileResponse
 from django.contrib.auth.decorators import login_required
-from django.core.exceptions import PermissionDenied
+from django.core.exceptions import PermissionDenied, ValidationError
 from django.http.request import HttpRequest
 from django.contrib import messages
 
@@ -28,15 +28,83 @@ def index(request: HttpRequest):
 
 def file(request: HttpRequest, id: str):
     """Return a file with a given id."""
-    if request.method == "POST":
-        print(request.FILES)
-        models.File()
+    file = models.File.objects.filter(name=id).first()
 
-        # file = models.File()
-    else:
-        # if
+    if not file:
+        file = models.File.objects.filter(id=id).first()
 
-        return FileResponse()
+        if file:
+            if file.name:
+                url = request.scheme + "://" + request.get_host() + "/file/" + file.name
+                print(url)
+                return redirect(url)
+
+    if request.method == "GET":
+
+        if not file:
+            return HttpResponse("File does not exist.", status=404)
+
+        if file.public:
+            file.update_read()
+            return FileResponse(file.file, filename=file.name)
+
+        if not request.user.is_authenticated:
+            return redirect("login", permanent=False)
+
+        print("user:", request.user)
+
+        permissions = models.Permission.objects.filter(
+            file=file, user=request.user
+        ).first()
+
+        permissions_choices = list(
+            map(lambda permission: permission.permission, permissions)
+        )
+
+        if models.PermissionChoices.READ not in permissions_choices:
+            return HttpResponse("No permission to read", status=403)
+
+        file.update_read()
+        return FileResponse(file.file, filename=file.name)
+
+    elif request.method == "POST":
+
+        if file:
+
+            permissions_choices = list(
+                map(lambda permission: permission.permission, permissions)
+            )
+
+            if models.PermissionChoices.WRITE not in permissions_choices:
+                return HttpResponse("No permission to write.", status=403)
+
+            file.update_write()
+
+        else:
+            file = models.File(author=request.user)
+
+        file.file = request.FILES["file"][1]
+
+        name = request.POST.get("name")
+        description = request.POST.get("description")
+        public = request.POST.get("public")
+        expiration = request.POST.get("expiration")
+
+        if name:
+            file.name = name
+
+        if description:
+            file.description = description
+
+        if public is not None:
+            file.public = public
+
+        if expiration:
+            file.expiration = expiration
+
+        file.save()
+
+    return HttpResponse("Only POST and GET requests are accepted!", status=500)
 
 
 @login_required
@@ -67,8 +135,7 @@ def access_index(request: HttpRequest):
         token.save()
         return HttpResponse(token.id)
 
-    if request.method == "POST":
-        print("test")
+    elif request.method == "POST":
         if not request.POST["token"]:
             messages.add_message(request, messages.WARNING, "Token is required.")
             return redirect("f")
@@ -84,15 +151,14 @@ def access_index(request: HttpRequest):
         description = request.POST.get("description")
         public = request.POST.get("public")
 
+        print("public:", public)
+
         # for filestream in request.FILES.getlist("file"):
 
         filestream = request.FILES.popitem()[1][0]
         name = filestream.name
-        print(name, filestream)
-        print(type(filestream))
 
         file = models.File.objects.filter(name=name).first()
-        print(file)
 
         if file:
             file.file = filestream
@@ -100,17 +166,17 @@ def access_index(request: HttpRequest):
             file = models.File(name=name, author=author)
             file.file = filestream
             print(file.file)
-            file.save()
 
-            if file_expiration:
-                file.expiration = file_expiration
-            if description:
-                file.description = description
-            if public:
-                file.public = public
+        if file_expiration:
+            file.expiration = file_expiration
+        if description:
+            file.description = description
+        if public is not None:
+            file.public = public
 
-            print(file.file)
-            file.save()
+        print("file.public:", file.public)
+
+        file.save()
 
         access = models.Access(file=file)
 
@@ -126,44 +192,60 @@ def access_index(request: HttpRequest):
 
         return HttpResponse(request.get_host() + "/f/" + access.id)
 
-        return HttpResponse("No file received", status=500)
+    return HttpResponse("Only POST and GET requests are accepted!", status=500)
 
 
+@csrf_exempt
 def access(request: HttpRequest, uuid: str):
     """Give a permission using a public url."""
     if request.method == "GET":
 
         token_id = request.GET.get("token")
-        token = models.Token.objects.filter(id=token_id).first()
+        try:
+            token = models.Token.objects.filter(id=token_id).first()
+        except ValidationError as e:
+            return HttpResponse("Unknown token.", status=404)
 
         access = models.Access.objects.filter(id=uuid).first()
+        if not access:
+            return HttpResponse("Unknown access.", status=404)
 
-        print(access.timestamp)
-        print(access.expiration)
-        print(access.timestamp + access.expiration)
+        print("timestamp:", access.timestamp)
+        print("expiration:", access.expiration)
+        print("expiration timestamp:", access.timestamp + access.expiration)
 
-        print(access.file.public)
+        print("public:", access.file.public)
+        print("access:", access)
 
         if access.file.public:
-            print(access.file.file)
+            print("file:", access.file.file)
             # return HttpResponse("test")
-            return FileResponse(access.file.file)
+            return FileResponse(access.file.file, filename=file.name)
+
+        print("token:", token)
 
         if not token:
             return HttpResponse("Token authentication required.", status=403)
 
         permissions = models.Permission.objects.filter(
-            file=access.file, user=request.user
-        ).all()
+            file=access.file, user=token.user
+        )
+
+        print("permissions:", permissions)
+
+        print(bool(permissions))
 
         if not permissions and token.user != access.file.author:
             access.count += 1
+            print("access.count:", access.count)
 
             if access.count > access.max_count:
                 access.active = False
                 return HttpResponse("Access request count exceeded.", status=403)
 
-            if access.timestamp + access.expiration > datetime.datetime.now():
+            print(access.timestamp + access.expiration)
+            print(timezone.now())
+            if access.timestamp + access.expiration < timezone.now():
                 access.active = False
                 return HttpResponse("Access expired.", status=403)
 
@@ -171,10 +253,13 @@ def access(request: HttpRequest, uuid: str):
                 return HttpResponse("Access closed.", status=403)
 
             permission = models.Permission(
-                file=access.file, user=request.user, permission=access.permission
+                file=access.file, user=token.user, permission=access.permission
             )
             permission.save()
 
+        print("access.file:", access.file)
         access.file.update_read()
 
-        return FileResponse(access.file)
+        return FileResponse(access.file.file, filename=access.file.name)
+
+    return HttpResponse("Only GET requests are accepted!", status=500)
