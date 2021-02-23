e85d93d9eee8d7811b2e59bd99d26a9e8cde6b31
root <root@localhost.localdomain>
2021-02-23 23:16:41 +0000

    no planning anymore

--- planning/urls.py
+++ planning/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+# from . import views
 
 urlpatterns = [
-    path("", views.planning, name="planning"),
+    # path("", views.planning, name="planning"),
 ]
