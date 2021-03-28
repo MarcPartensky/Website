df97c4faf81afa53272a01c581a319ab7e4e19b6
marc <marc.partensky@gmail.com>
2021-03-28 16:49:11 +0000

    better commands in run.sh

--- run.sh
+++ run.sh
@@ -1,30 +1,28 @@
 #!/bin/sh
+source .env
 
-run_with_ssl() {
-	"daphne -e ssl:443:privateKey=$KEY:certKey=$CERT django_project.asgi:application"
-}
-
-run() {
+run_bg() {
 	daphne -p $1 django_project.asgi:application > /dev/null &
 }
 
 mysql() {
-	source .env
+	echo -e "Starting \e[92mMySQL\e[0m"
 	docker-compose up -d mysql
 }
 
 redis() {
-	source .env
+	echo -e "Starting \e[92mRedis\e[0m"
 	docker-compose up -d redis
 }
 
 django() {
-	source .env
-	echo -e "Starting \e[33mDjango\e[0m"
+	echo -e "Starting \e[92mDjango\e[0m"
 	# ./manage.py runserver 0.0.0.0:8000
-	export REDIS_URL="localhost:6379"
-	daphne -p 8000 -b 0.0.0.0 django_project.asgi:application
+	# daphne -p 8000 -b 0.0.0.0 django_project.asgi:application
+	daphne -e "ssl:${PORT}:privateKey=${KEY}:certKey=${CERT}" django_project.asgi:application
 }
+
+alias run=django
 
 up() {
 	mysql
@@ -34,20 +32,18 @@ up() {
 
 down() {
 	docker-compose down
-	kill %
+	echo -e "Ended \e[31mRedis\e[0m"
+	echo -e "Ended \e[31mMySQL\e[0m"
+	kill % && echo -e "Ended \e[31mDjango\e[0m"	&& echo -e "Ended \e[31mFTP\e[0m"
 }
 
 ftp() {
-	./manage.py ftpserver 127.0.0.1:10021
+	echo -e "Starting \e[92mFTP Server\e[0m"
+	./manage.py ftpserver $FTP_URL
 }
 
-run_all() {
-	trap 'kill $BGPID; exit' INT
-	# background command
-	echo -e "Starting \e[33mDjango\e[0m"
-	daphne -p 80 django_project.asgi:application &
-	BGPID=$!
-	# foreground command of the script
-	echo -e "Starting \e[32mFtp Server"
-	./manage.py ftpserver 127.0.0.1:10021
+all() {
+	ftp &
+	up
+	down
 }
