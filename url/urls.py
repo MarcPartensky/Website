d68080e0abe9ca26812f51c089a070a2d7c77db8
marc <marc.partensky@gmail.com>
2021-06-01 16:17:43 +0000

    double slash for shortener

--- url/urls.py
+++ url/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path("urls", views.urls, name="urls"),
     path("requests", views.requests, name="requests"),
-    path("u", views.url, name="url-simple"),
+    path("u", views.url, name="url-simple-u"),
+    path("/", views.url, name="url-simple-slash"),
+    path("/<str:route>", views.url, name="url-slash"),
     path("u<str:route>", views.url, name="url"),
 ]
