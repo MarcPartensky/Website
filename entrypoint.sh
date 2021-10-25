22db8384971ec6ef066002120f1da3d0c5aef2b5
marc <marc.partensky@gmail.com>
2021-10-25 18:23:04 +0200

    entrypoint.sh without makemigrations and with createsuperuser

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-./manage.py makemigrations
 ./manage.py migrate
-daphne django_project.asgi:application --port 80 --bind website -v2
+./manage.py createsuperuser --noinput
+daphne django_project.asgi:application --port 80 --bind 127.0.0.1 -v2
