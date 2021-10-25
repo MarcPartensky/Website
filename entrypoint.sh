04350a89ee69e1d2ed5a15c353610bfa76735205
marc <marc.partensky@gmail.com>
2021-10-25 16:41:53 -0400

    entrypoint that works

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
-echo -e 'Running \033[1mentrypoint.sh\033[0m'
+port=${1:-$PORT}
+addr=${2:-$ADDR}
+port=${port:-"80"}
+addr=${addr:-"127.0.0.1"}
+username=${USERNAME:-"admin"}
+email=${EMAIL:-"admin@admin.com"}
+DJANGO_SUPERUSER_PASSWORD=${PASSWORD:-$(date +%s | sha256sum | base64 | head -c 32)}
+
+echo -e "Running \033[1mentrypoint.sh\033[0m using $ADDR:$PORT"
 ./manage.py migrate
-./manage.py createsuperuser --noinput
-daphne django_project.asgi:application --port 80 --bind 127.0.0.1 -v2
+echo -e "Creating \033[1msuper user\033[0m using:\n - username: \033[1m$username\033[0m\n - email: \033[1m$email\033[0m\n"
+./manage.py createsuperuser --user $username --email $email --noinput
+daphne django_project.asgi:application --port $port --bind $addr -v2
