c620ee05f2e1e6c9a7a9bf2c1d868f5a3fbb8fe7
marc <marc.partensky@gmail.com>
2021-11-29 00:51:01 +0100

    new entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,13 @@ port=${1:-$PORT}
 addr=${2:-$ADDR}
 port=${port:-"80"}
 addr=${addr:-"127.0.0.1"}
-username=${USERNAME:-"admin"}
-email=${EMAIL:-"admin@admin.com"}
+username=${DJANGO_SUPERUSER_USERNAME:-"admin"}
+email=${DJANGO_SUPERUSER_EMAIL:-"admin@admin.com"}
 DJANGO_SUPERUSER_PASSWORD=${PASSWORD:-$(date +%s | sha256sum | base64 | head -c 32)}
 
 echo -e "Running \033[1mentrypoint.sh\033[0m using $ADDR:$PORT"
 ./manage.py migrate
 echo -e "Creating \033[1msuper user\033[0m using:\n - username: \033[1m$username\033[0m\n - email: \033[1m$email\033[0m\n"
 ./manage.py createsuperuser --user $username --email $email --noinput
-./manage.py collecstatic --noinput
+./manage.py collecstatic --noinput --clear
 daphne django_project.asgi:application --port $port --bind $addr -v2
