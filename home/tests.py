345312a259de0573b5069a107d79432652ac1487
Marc Partensky <marc.partensky@gmail.com>
2022-01-17 18:57:27 +0100

    testing homepage

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,21 @@
+import os
 from django.test import TestCase
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 
-# Create your tests here.
+
+class AnimalTestCase(TestCase):
+    def setUp(self):
+        """Setup selenium."""
+        options = Options()
+        options.headless = True
+        self.driver = webdriver.Firefox(options=options)
+        host = os.environ["HOST"]
+        port = os.environ["PORT"]
+        self.url = f"{host}:{port}"
+
+    def test_home_page_loads(self):
+        """Animals that can speak are correctly identified"""
+        self.driver.get(self.url)
+        icon = self.driver.find_element_by_id("img-theme")
+        icon.click()
