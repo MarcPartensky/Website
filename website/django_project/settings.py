5ec7bd1021161c25a1a0f2e44a2209d2f4935c63
Marc Partensky <marc.partensky@gmail.com>
2022-08-20 20:09:01 +0200

    updated and it works now

--- website/django_project/settings.py
+++ website/django_project/settings.py
@@ -36,6 +36,7 @@ HEROKU_SERVER = os.environ.get("HEROKU_SERVER")
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print("BASE_DIR:", BASE_DIR)
 
 # Allowing cors
 CORS_ORIGIN_ALLOW_ALL = False
@@ -286,9 +287,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-
 STATIC_URL = "/static/"
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
