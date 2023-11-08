1d03de2913b67e6a5cbfe7975a6c3a70e2cf800d
Marc Partensky <marc.partensky@gmail.com>
2023-11-08 18:33:41 +0100

    back to slash

--- website/url/urls.py
+++ website/url/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path("urls", views.urls, name="urls"),
     path("requests", views.requests, name="requests"),
     path("u", views.url, name="url-simple-u"),
-    path("", views.url, name="url-simple-slash"),
-    path("<str:route>", views.url, name="url-slash"),
+    path("/", views.url, name="url-simple-slash"),
+    path("/<str:route>", views.url, name="url-slash"),
     path("u<str:route>", views.url, name="url"),
 ]
